c6c5ffb3ee91a02776dd89c91d44379237da261a
Dhananjaya <k.dhananjaya88@gmail.com>
2026-05-10 11:07:22 +0530

    first commit

---
+++ tests/login.spec.js
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+const path = require('path');
+
+test('login success test', async ({ page }) => {
+    //open local html file
+    const filePath = path.join(__dirname, '..', 'login.html');
+    await page.goto('file://' + filePath);
+
+    //perform action
+    await page.fill('#username', 'admin');
+    await page.fill('#password', 'password');
+    await page.click('button');
+
+    //assertion
+    await expect(page.locator('#message')).toBeVisible();
+    await expect(page.locator('#message')).toHaveText('Login successful!');
+});
